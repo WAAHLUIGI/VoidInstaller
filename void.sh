0a43f49e594e66a8cf9330269321710a66c70e44
WAAHLUIGI <76225961+WAAHLUIGI@users.noreply.github.com>
2021-03-11 09:02:07 +0300

    Add files via upload

---
+++ void.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+#fetch the rootfs tarball, the sha256sums and the signatures
+
+wget https://alpha.de.repo.voidlinux.org/live/current/void-x86_64-ROOTFS-20210218.tar.xz
+wget https://alpha.de.repo.voidlinux.org/live/current/sha256sum.sig
+wget https://alpha.de.repo.voidlinux.org/live/current/sha256sum.txt
+wget https://raw.githubusercontent.com/void-linux/void-packages/master/srcpkgs/void-release-keys/files/void-release-20210218.pub
+
+
+# Verify the image for security
+
+
+signify -C -p void-release-20210218.pub -x sha256sum.sig void-x86_64-ROOTFS-20210218.tar.xz
+sha256sum -c --ignore-missing sha256sum.txt
+
+echo "What partition do you want to install Void on? \c"
+read PARTITION
+
+mkdir voidinstall
+mount $PARTITION ./voidinstall/
+rm -rf voidinstall/*
+tar xvf void-x86_64-ROOTFS-20210218.tar.xz -C voidinstall
+mount --rbind /sys voidinstall/sys && mount --make-rslave voidinstall/sys
+mount --rbind /dev voidinstall/dev && mount --make-rslave voidinstall/dev
+mount --rbind /proc voidinstall/proc && mount --make-rslave voidinstall/proc
+echo "nameserver 192.168.1.1" > voidinstall/etc/resolv.conf
+echo "xbps-install -Syu xbps;  xbps-install -yu;  xbps-install -y base-system;  xbps-remove -y base-voidstrap; xbps-install -y grub; xbps-install -y xfce4; xbps-install -y vim; ln -s /etc/sv/dhcpcd /var/service/; ln -s /etc/sv/alsa /var/service/; passwd; " >> voidinstall/hell.sh & chmod +x voidinstall/hell.sh
+PS1='(chroot) # ' chroot voidinstall/ ./hell.sh
+# ask the user what filesystem they want to use on it, use mkfs. accordingly, then do an (optional) bad block check using fsck -vcck
+
+
